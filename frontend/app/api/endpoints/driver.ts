80f12b27f097244f6f64cbd7cf195b47233761a1
Elkwizard <bananamanthefourth@gmail.com>
2026-03-05 23:41:45 -0800

    added new driver endpoints to API wrapper

--- frontend/app/api/endpoints/driver.ts
+++ frontend/app/api/endpoints/driver.ts
@@ -7,6 +7,27 @@ class DriverApi extends CrudApi<Driver> {
   constructor() {
     super('/driver', json.driver);
   }
+  async fromToken(token: string) {
+    try {
+      const driver: json.JSONDomainObject<Driver> = await fetchJSON(
+        'GET',
+        `${this.resource}/token/${token}`,
+      );
+      return json.driver.parse(driver);
+    } catch (err) {
+      this.error(`Failed to read driver by token; token=${token}`, err);
+      return null;
+    }
+  }
+  async markReturned(token: string) {
+    try {
+      await fetchEndpoint('PUT', `${this.resource}/returned/${token}`);
+      return true;
+    } catch (err) {
+      this.error(`Failed to mark driver as returned; token=${token}`, err);
+      return false;
+    }
+  }
   async setOnShift({id}: Driver['id'], onShift: boolean) {
     try {
       await fetchEndpoint(
